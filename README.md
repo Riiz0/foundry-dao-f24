e7d86a5d2cedd31866f703686d4ed4542a51ea51
Riiz0 <shawnrizo@hotmail.com>
2024-04-24 19:31:17 -0400

    1. Contract Complete Setup, Test Complete

--- README.md
+++ README.md
@@ -1,3 +1,115 @@
+# Foundry DAO Governance
+
+## About
+
+1. We are going to have a contract controlled by a DAO
+2. Every transaction that the DAO wants to send has to be voted on
+Please note: ERC20 based voting is not always recommended, and I encourage you to explore other forms of governance like reputation based or "skin-in-the-game" based.
+Plutocracy is bad! Don't default to ERC20 token voting!!
+
+- [Foundry DAO Governance](#foundry-dao-governance)
+  - [About](#about)
+- [Getting Started](#getting-started)
+  - [Requirements](#requirements)
+  - [Quickstart](#quickstart)
+- [Usage](#usage)
+  - [Test](#test)
+  - [Deploy](#deploy)
+  - [Deploy - Other Network](#deploy---other-network)
+  - [Testing](#testing)
+  - [Estimate Gas](#estimate-gas)
+- [Formatting](#formatting)
+  - [Foundry](#foundry)
+  - [Documentation](#documentation)
+  - [Usage](#usage-1)
+    - [Build](#build)
+    - [Test](#test-1)
+    - [Format](#format)
+    - [Gas Snapshots](#gas-snapshots)
+    - [Anvil](#anvil)
+    - [Deploy](#deploy-1)
+    - [Cast](#cast)
+    - [Help](#help)
+
+# Getting Started
+
+## Requirements
+
+- [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+  - You'll know you did it right if you can run `git --version` and you see a response like `git version x.x.x`
+- [foundry](https://getfoundry.sh/)
+  - You'll know you did it right if you can run `forge --version` and you see a response like `forge 0.2.0 (816e00b 2023-03-16T00:05:26.396218Z)`
+
+## Quickstart
+
+```
+git clone https://github.com/Riiz0/foundry-defi-stablecoin-f24.git
+cd foundry-defi-stablecoin-f23
+forge build
+```
+
+# Usage
+
+## Test
+
+```
+forge test
+```
+
+## Deploy
+
+I did not write deploy scripts for this project, you can if you'd like!
+
+This will default to your local node. You need to have it running in another terminal in order for it to deploy.
+
+```
+make deploy
+```
+
+## Deploy - Other Network
+
+[See below](#deployment-to-a-testnet-or-mainnet)
+
+## Testing
+
+We talk about 4 test tiers in the video.
+
+1. Unit
+2. Integration
+3. Forked
+4. Staging
+
+```
+forge test
+```
+
+```
+forge coverage
+```
+
+and for coverage based testing:
+
+```
+forge coverage --report debug
+```
+
+## Estimate Gas
+
+You can estimate how much gas things cost by running:
+
+```
+forge snapshot
+```
+And you'll see and output file called `.gas-snapshot`
+
+# Formatting
+
+To run code formatting:
+
+```
+forge fmt
+```
+
 ## Foundry
 
 **Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
---
+++ test/MyGovernorTest.t.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MyGovernor} from "../src/MyGovernor.sol";
+import {GovToken} from "../src/GovToken.sol";
+import {Box} from "../src/Box.sol";
+import {TimeLock} from "../src/TimeLock.sol";
+
+contract MyGovernorTest is Test {
+    MyGovernor governor;
+    GovToken govToken;
+    Box box;
+    TimeLock timeLock;
+
+    address public USER = makeAddr("user");
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
+    uint256 public constant MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
+    uint256 public constant QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
+    uint256 public constant VOTING_PERIOD = 50400; // This is how long voting lasts
+    uint256 public constant VOTING_DELAY = 1; // How many blocks till a proposal vote becomes active
+
+    address[] proposers;
+    address[] executors;
+
+    uint256[] values;
+    bytes[] calldatas;
+    address[] targets;
+
+    function setUp() public {
+        govToken = new GovToken();
+        govToken.mint(USER, INITIAL_SUPPLY);
+
+        vm.prank(USER);
+        govToken.delegate(USER);
+        timeLock = new TimeLock(MIN_DELAY, proposers, executors, address(this));
+        governor = new MyGovernor(govToken, timeLock);
+
+        bytes32 proposerRole = timeLock.PROPOSER_ROLE();
+        bytes32 executorRole = timeLock.EXECUTOR_ROLE();
+        bytes32 adminRole = timeLock.DEFAULT_ADMIN_ROLE();
+
+        timeLock.grantRole(proposerRole, address(governor));
+        timeLock.grantRole(executorRole, address(0));
+        timeLock.revokeRole(adminRole, address(this));
+        box = new Box(address(timeLock));
+    }
+
+    function testCantUpdateBoxWithoutGovernance() public {
+        vm.expectRevert();
+        box.store(1);
+    }
+
+    function testGovernanceUpdatesBox() public {
+        uint256 valueToStore = 888;
+        string memory description = "store 1 in Box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature("store(uint256)", valueToStore);
+        values.push(0);
+        calldatas.push(encodedFunctionCall);
+        targets.push(address(box));
+
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        console.log("Proposal State: ", uint256(governor.state(proposalId)));
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        console.log("Proposal State: ", uint256(governor.state(proposalId)));
+
+        string memory reason = "cuz blue frog is cool";
+        uint8 voteWay = 1; //Voting For Yes
+        vm.prank(USER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        vm.warp(block.timestamp + MIN_DELAY + 1);
+        vm.roll(block.number + MIN_DELAY + 1);
+
+        governor.execute(targets, values, calldatas, descriptionHash);
+
+        assert(box.getNumber() == valueToStore);
+        console.log("Box value: ", box.getNumber());
+    }
+}
